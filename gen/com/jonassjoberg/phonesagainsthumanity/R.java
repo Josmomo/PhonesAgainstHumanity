bac4a59e26332bb8a90bbb69fd547f0613001915
Hugo Nissar <nissar.hugo@gmail.com>
2015-01-14 23:07:26 +0100

    One more background image

--- gen/com/jonassjoberg/phonesagainsthumanity/R.java
+++ gen/com/jonassjoberg/phonesagainsthumanity/R.java
@@ -23,10 +23,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int black_card=0x7f020001;
-        public static final int buttons=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int white_card=0x7f020004;
+        public static final int background_no_text=0x7f020001;
+        public static final int black_card=0x7f020002;
+        public static final int buttons=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int white_card=0x7f020005;
     }
     public static final class id {
         public static final int ListView01=0x7f080005;
