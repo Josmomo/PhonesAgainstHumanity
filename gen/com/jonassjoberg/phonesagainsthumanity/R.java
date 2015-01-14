6c6879046effe7f828c64ca65a01b5a8648a97ee
Hugo Nissar <nissar.hugo@gmail.com>
2015-01-15 00:36:02 +0100

    Created a black card for the host

--- gen/com/jonassjoberg/phonesagainsthumanity/R.java
+++ gen/com/jonassjoberg/phonesagainsthumanity/R.java
@@ -30,22 +30,19 @@ public final class R {
         public static final int white_card=0x7f020005;
     }
     public static final class id {
-        public static final int ListView01=0x7f080005;
-        public static final int ListViewRespondCards=0x7f080007;
-        public static final int TextView01=0x7f080004;
-        public static final int action_settings=0x7f08000f;
-        public static final int buttonBluetoothSearch=0x7f08000a;
-        public static final int buttonDownload=0x7f08000e;
-        public static final int buttonHost=0x7f08000d;
-        public static final int buttonJoin=0x7f08000c;
-        public static final int listViewBluetoothConnectedDevices=0x7f080009;
-        public static final int listViewBluetoothSearchResult=0x7f08000b;
+        public static final int action_settings=0x7f08000c;
+        public static final int buttonBluetoothSearch=0x7f080007;
+        public static final int buttonDownload=0x7f08000b;
+        public static final int buttonHost=0x7f08000a;
+        public static final int buttonJoin=0x7f080009;
+        public static final int card_in_hand=0x7f080004;
+        public static final int listViewBluetoothConnectedDevices=0x7f080006;
+        public static final int listViewBluetoothSearchResult=0x7f080008;
         public static final int listViewCards=0x7f080001;
         public static final int listViewVoteCards=0x7f080003;
-        public static final int progressBarHostWaiting=0x7f080008;
+        public static final int progressBarHostWaiting=0x7f080005;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
-        public static final int textViewReceive=0x7f080006;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -101,6 +98,9 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int BlackCards=0x7f060004;
         public static final int ButtonText=0x7f060002;
+        public static final int Cards=0x7f060003;
+        public static final int WhiteCards=0x7f060005;
     }
 }
