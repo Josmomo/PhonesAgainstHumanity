1b20a8fdd69b574f7c4f3d099f22469e177287c0
Hugo Nissar <nissar.hugo@gmail.com>
2015-01-15 03:47:10 +0100

    Removed test code

--- src/com/jonassjoberg/phonesagainsthumanity/GameHostActivity.java
+++ src/com/jonassjoberg/phonesagainsthumanity/GameHostActivity.java
@@ -33,15 +33,6 @@ public class GameHostActivity extends Activity {
 		changeView = (Button) findViewById(R.id.change_view);
 		cardInHand = (TextView) findViewById(R.id.card_in_hand);
 		
-		
-		for(int i = 1; i <= 10; i++) {
-			String s = "";
-			for(int j = 0; j < i*i*i; j++) {
-				s += j + " ";
-			}
-			addCard(s);
-		}
-		
 	}
 
 	@Override
